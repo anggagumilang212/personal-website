e14d51636c77043ad67191de80860606eb33cba0
anggagumilang212 <anggagumilang212@gmail.com>
2023-10-21 10:27:14 +0700

    delete variable

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,8 +3,6 @@ import { Metadata } from 'next'
 import Container from '@/common/components/elements/Container'
 import PageHeading from '@/common/components/elements/PageHeading'
 import { METADATA } from '@/common/constant/metadata'
-import { prisma } from '@/common/libs/prisma'
-import { IProjectItem } from '@/common/types/projects'
 
 import Projects from '@/modules/projects'
 
@@ -21,7 +19,6 @@ const PAGE_TITLE = 'Portfolio Projects'
 const PAGE_DESCRIPTION = 'Projects that I have worked on with the team, whether as frontend, backend or full stack.'
 
 export default async function ProjectsPage() {
-  const projects = await getProjets()
   return (
     <>
       <Container data-aos="fade-up">
@@ -32,16 +29,3 @@ export default async function ProjectsPage() {
   )
 }
 
-async function getProjets(): Promise<IProjectItem[]> {
-  const response = await prisma.projects.findMany({
-    orderBy: [
-      {
-        is_featured: 'desc'
-      },
-      {
-        updated_at: 'desc'
-      }
-    ]
-  })
-  return response
-}
--- modules/projects/components/Projects.tsx
+++ modules/projects/components/Projects.tsx
@@ -10,8 +10,8 @@ export default function Projects() {
   <div className="container  py-24 mx-auto -mt-24">
     <div className="flex flex-wrap -m-7">
       <div className="p-4 w-full sm:w-1/2 lg:w-1/2">
-        <div className="transition ease-in-out delay-150 h-auto overflow-hidden lg:w-[400px] w-full cursor-pointer shadow-sm lg:hover:shadow-md dark:lg:hover:shadow-xl bg-white rounded-xl dark:bg-neutral-800 border-opacity-60" data-aos="fade-up">
-          <Image className="hover:scale-105 transition-all duration-300 object-cover object-center w-full lg:h-48 md:h-36" src="/git dan github.jpg" alt="blog" width={400} height={192} />
+        <div className="transition ease-in-out delay-150 h-auto overflow-hidden lg:w-[400px] w-full shadow-sm lg:hover:shadow-md dark:lg:hover:shadow-xl bg-white rounded-xl dark:bg-neutral-800 border-opacity-60" data-aos="fade-up">
+          <Image className="hover:scale-105 transition-all duration-300 cursor-pointer object-cover object-center w-full lg:h-48 md:h-36" src="/git dan github.jpg" alt="blog" width={400} height={192} />
           <div className="p-6">
             <h1 className="mb-3 text-lg font-medium text-gray-900 dark:text-white title-font">Laravel vs Codeigniter</h1>
             <p className="mb-3 leading-relaxed">Pada artikel ini akan membahas mengenai framework Laravel dan Codeigniter, dan bagaimana perbandingan dari kedua framework tersebut.</p>
@@ -27,8 +27,8 @@ export default function Projects() {
         </div>
       </div>
       <div className="p-4 w-full sm:w-1/2 lg:w-1/2">
-        <div className="transition ease-in-out delay-150 lg:w-[400px] h-auto overflow-hidden w-full cursor-pointer shadow-sm lg:hover:shadow-md dark:lg:hover:shadow-xl bg-white rounded-xl dark:bg-neutral-800 border-opacity-60" data-aos="fade-up">
-          <Image className="hover:scale-105 transition-all duration-300 object-cover object-center w-full lg:h-48 md:h-36" src="/git dan github.jpg" alt="blog" width={400} height={192} />
+        <div className="transition ease-in-out delay-150 lg:w-[400px] h-auto overflow-hidden w-full  shadow-sm lg:hover:shadow-md dark:lg:hover:shadow-xl bg-white rounded-xl dark:bg-neutral-800 border-opacity-60" data-aos="fade-up">
+          <Image className="hover:scale-105 transition-all duration-300 object-cover cursor-pointer object-center w-full lg:h-48 md:h-36" src="/git dan github.jpg" alt="blog" width={400} height={192} />
           <div className="p-6">
             <h1 className="mb-3 text-lg font-medium text-gray-900 dark:text-white title-font">Laravel vs Codeigniter</h1>
             <p className="mb-3 leading-relaxed">Pada artikel ini akan membahas mengenai framework Laravel dan Codeigniter, dan bagaimana perbandingan dari kedua framework tersebut.</p>
