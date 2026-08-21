c7f28dabba5ea32460f571b6098ad860c2e488a5
anggagumilang212 <anggagumilang212@gmail.com>
2026-08-21 14:01:24 +0700

    fix: remove dev.to api call in view service to prevent server side exception

--- services/view.ts
+++ services/view.ts
@@ -1,19 +1,6 @@
 import axios from 'axios'
-
 import { BlogItem } from '@/common/types/blog'
 
 export async function getBlogViews(searchParams: string) {
-  const URL = `https://dev.to/api/articles/me/all`
-  const response = await axios.get(URL, {
-    headers: {
-      'api-key': process.env.DEVTO_KEY
-    }
-  })
-  if (response.status !== 200) return 0
-  const data = response.data
-
-  if (!searchParams) return 0
-  const findArticle = data?.find((blog: BlogItem) => blog.id === parseInt(searchParams))
-  const page_views_count: number = findArticle?.page_views_count || 0
-  return page_views_count
+  return 0
 }
